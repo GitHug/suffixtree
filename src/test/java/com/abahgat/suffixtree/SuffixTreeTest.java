5df70b168f28ee7aedeb0a02020bc59c71d89197
Fredrik <fredrik.makila@gmail.com>
2013-09-17 14:42:59 +0900

    Added testcases for the new methods

--- src/test/java/com/abahgat/suffixtree/SuffixTreeTest.java
+++ src/test/java/com/abahgat/suffixtree/SuffixTreeTest.java
@@ -15,11 +15,20 @@
  */
 package com.abahgat.suffixtree;
 
+import static com.abahgat.suffixtree.Utils.getSubstrings;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import junit.framework.TestCase;
-import static com.abahgat.suffixtree.Utils.getSubstrings;
 
 public class SuffixTreeTest extends TestCase {
 
@@ -218,5 +227,116 @@ public class SuffixTreeTest extends TestCase {
         Collection<String> ret = getSubstrings("wrd");
         assertTrue(ret.equals(exp));
     }
+    
+    //Helper method for testSearchMatchingSuffix
+    private Set<String> naiveSuffixGen(String[] arr, String regex) {
+		Set<String> complete = new HashSet<String>();
+		for(int i = 0; i < arr.length; i++) {
+			String comp = "";
+			String t = arr[i];
+			for(int j = t.length() - 1; j >= 0; j--) {
+				comp = t.charAt(j) + comp;
+				complete.add(comp);
+			}
+		}
+	
+ 		Iterator<String> it = complete.iterator();
+		while(it.hasNext()) {
+			String s = it.next();
+			if(!s.matches(regex)) {
+				it.remove();
+			}
+		}
+		return complete;
+	}
+    
+    public void testSearchMatchingSuffix() {
+    	//Matches a string containing two $c 
+    	String regex = "(^|.*?\\s)\\$c.*?\\s\\$c[^A-Za-z0-9_$].*?$";
+    	
+		String[] arr = {
+			"$c or $c?", 
+			"$c is much better than $c", 
+			"15 $c is cooler than that!", 
+			"If I had 5$ for everytime I heard that, I would have $5", 
+			"Two $c for the price of $c",
+			"Peanut butter jelly", 
+			" ", 
+			"$$$$$$ #end", 
+			"Coconut banana power!", 
+			"I am almost as good in $c as in $c."
+		};
+    	
+		Set<String> complete = naiveSuffixGen(arr, regex);
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+		
+		for(int i = 0; i < arr.length; i++) {
+			tree.put(arr[i], i);
+		}
+		
+		Set<String> set = tree.searchMatchingSuffix(regex);
+		List<String> treeList = new ArrayList<String>(set);
+		List<String> completeList = new ArrayList<String>(complete);
+		
+		Collections.sort(treeList);
+		Collections.sort(completeList);
+		
+		assertArrayEquals(treeList.toArray(new String[treeList.size()]), completeList.toArray(new String[completeList.size()]));
+		
+		//Matches a string containing two C
+		String regex1 = "^.*?C.*?C.*?$";
+						
+		String[] test1 = {
+			"ABBCAABBAABBACAAAB",
+			"CC",
+			"ABCABC",
+			"CAC",
+			"ABBBABB",
+			"ABC",
+			"AAAABBABABBACC",
+			""	
+		};
+		
+		Set<String> comp = naiveSuffixGen(test1, regex1);
+		GeneralizedSuffixTree ki = new GeneralizedSuffixTree();
+		
+		for(int i = 0; i < test1.length; i++) {
+			ki.put(test1[i], i);
+		}
+		
+		Set<String> col = ki.searchMatchingSuffix(regex1);
+		List<String> kiList = new ArrayList<String>(col);
+		List<String> compList = new ArrayList<String>(comp);
+		
+		Collections.sort(kiList);
+		Collections.sort(compList);
+		
+		
+		assertArrayEquals(kiList.toArray(new String[kiList.size()]), compList.toArray(new String[compList.size()]));
+				
+    }
+    
+    public void testCreateDotGraph() {
+    	String[] arr = {
+    			"ABBCAABBAABBACAAAB",
+    			"CC",
+    			"ABCABC",
+    			"CAC",
+    			"ABBBABB",
+    			"ABC",
+    			"AAAABBABABBACC",
+    			""	
+    		};
+    	
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+		for(int i = 0; i < arr.length; i++) {
+			tree.put(arr[i], i);
+		}
+		
+		tree.createDotGraph();
+		
+		File f = new File("out.gv");
+		assertTrue(f.exists());
+	}
 
 }
